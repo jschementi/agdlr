bc4463f112109edfabe7a85a53e83a20f934cbcb
Jimmy Schementi <jschementi@gmail.com>
2009-02-23 10:24:22 -0800

    Run any valid integration tests

--- test/tests/integration/test_args.rb
+++ test/tests/integration/test_args.rb
@@ -1,7 +1,6 @@
 describe "args - Python splat args" do
   
   it 'call exec to create f(*args)' do
-    require 'python'
     python "exec 'def f(*args): return repr(args)'"
     python("f(1,2,3,4)", :expression).
       should.equal '(1, 2, 3, 4)'.to_clr_string
